1ea95d8a6cbc50aeec28b3e6f18faa1065d00a26
b-chavez <brandedhard@gmail.com>
2020-08-08 14:15:59 -0700

    added some JS and a button to call JS

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+let myDivTotalDue = document.getElementById("totalDue");
+myDivTotalDue.innerHTML = "Banana!"
+
+
+
+
+
+function clickButton(){
+    let myTotalDos = document.getElementById("total");
+    let myTips = document.getElementById("tips");
+
+    tipCalc(myTotalDos.nodeValue, myTips.nodeValue)
+}
+
 function tipCalc(total, percent){
     let myTip, myTotal = 0;
     myTip = total * (percent * .01);
     myTotal = total + parseFloat(myTip.toFixed(2));
-    return "Your tip amount is " + myTip.toFixed(2) + ". Your toal due is " + myTotal; 
+    myDivTotalDue.innerHTML = "Your tip amount is " + myTip.toFixed(2) + ". Your toal due is " + myTotal; 
 }
