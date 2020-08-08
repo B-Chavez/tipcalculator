8b54520959ce2cae2526fdce18adccaa75ef919d
b-chavez <brandedhard@gmail.com>
2020-08-08 12:00:30 -0700

    JS function is made

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function tipCalc(total, percent){
     let myTip, myTotal = 0;
     myTip = total * (percent * .01);
-    myTotal = total + myTip.toFixed(2);
+    myTotal = total + parseFloat(myTip.toFixed(2));
     return "Your tip amount is " + myTip.toFixed(2) + ". Your toal due is " + myTotal; 
 }
