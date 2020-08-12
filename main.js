c5ffc9aeb676749d7ed7c20607ca6fdd1ac5b92f
b-chavez <brandedhard@gmail.com>
2020-08-12 16:37:42 -0700

    final JS touches and it works

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 let myDivTotalDue = document.getElementById("totalDue");
 myDivTotalDue.innerHTML = "Banana!"
-
+//Practicing to see what I can do to change values on elements
 
 
 
 
 function clickButton(){
-    let myTotalDos = document.getElementById("total");
-    let myTips = document.getElementById("tips");
-
-    tipCalc(myTotalDos.nodeValue, myTips.nodeValue)
+    let myTotalDos = document.getElementById("total").value;
+    let myTips = document.getElementById("tips").value;
+    //
+    tipCalc(parseFloat(myTotalDos), parseFloat(myTips))
 }
 
 function tipCalc(total, percent){
     let myTip, myTotal = 0;
     myTip = total * (percent * .01);
     myTotal = total + parseFloat(myTip.toFixed(2));
-    myDivTotalDue.innerHTML = "Your tip amount is " + myTip.toFixed(2) + ". Your toal due is " + myTotal; 
+    myDivTotalDue.innerHTML = "Your tip amount is " + myTip.toFixed(2) + ". Your toal due is " + myTotal.toFixed(2); 
 }
