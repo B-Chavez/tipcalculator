f4d62d419fa0b3c2a1c2b5d1957f390f68485b3c
b-chavez <brandedhard@gmail.com>
2020-09-10 21:22:37 -0700

    Removed innerHTML and onclick

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let myDivTotalDue = document.getElementById("totalDue");
-myDivTotalDue.innerHTML = "Banana!"
+myDivTotalDue.textContent = "Banana!"
 //Practicing to see what I can do to change values on elements
 
-
+document.querySelector("button").addEventListener('click', function(){clickButton})
 
 
 function clickButton(){
@@ -16,5 +16,5 @@ function tipCalc(total, percent){
     let myTip, myTotal = 0;
     myTip = total * (percent * .01);
     myTotal = total + parseFloat(myTip.toFixed(2));
-    myDivTotalDue.innerHTML = "Your tip amount is " + myTip.toFixed(2) + ". Your toal due is " + myTotal.toFixed(2); 
+    myDivTotalDue.textContent = "Your tip amount is " + myTip.toFixed(2) + ". Your toal due is " + myTotal.toFixed(2); 
 }
